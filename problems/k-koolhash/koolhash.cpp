880d23b285882881cb3f295975a65afbb132cdf5
Dylan Irlbeck <irlbeck2@gelib-4c-36.ews.illinois.edu>
2019-04-09 23:09:09 -0500

    Finished test data for koolhash

--- problems/k-koolhash/koolhash.cpp
+++ problems/k-koolhash/koolhash.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
 using namespace std;
 
 int main() {
   int n, temp;
   cin >> n;
-  while (n--) {
+  while(n--) {
      cin >> temp;
      int leastSig = temp & 1, mostSig = temp & (1<<31), numSig = 0;
      while (temp) {
